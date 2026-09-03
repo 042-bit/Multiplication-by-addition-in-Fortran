267b0d427d6338c70e9382b874a3109396fbadc1
042 <kokosk399@gmail.com>
2026-09-03 22:48:54 +0200

    Add Fortran program to multiply two numbers

---
+++ multiple.f95
@@ -0,0 +1,24 @@
+program multiply
+implicit none 
+
+integer :: first_number = 0
+integer :: second_number = 0 
+integer :: counter = 0 
+integer :: buffer = 0
+integer :: bufer_two = 0 
+
+
+read *, first_number
+read *, second_number
+
+    do counter = bufer_two, second_number
+    
+        buffer = buffer + first_number
+    
+    end do
+
+buffer = buffer - first_number
+
+print *, buffer
+
+end program multiply
